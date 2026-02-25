1565c5868ea408d222cbf8412aa434bc06fcad56
eduardoesilva09-design <eduardo.e.silva09@escola.pr.gov.br>
2026-02-25 13:28:29 +0000

    variaveis para pessoa

---
+++ variaveis/var-pessoa.js
@@ -0,0 +1,18 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Ichigo"   //string
+let sobrenome = "Kurosaki"  //string
+const cpf = "15105717-75"  //string
+const rg = "151057 ssp"   //string
+let idade = 17            //number
+let solteiro = true       //boolean
+let nota  = 5 + 3         //number
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
