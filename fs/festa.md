b25ee4dbf04b652eebc4dda4e9a34110661661e8
eduardoesilva09-design <eduardo.e.silva09@escola.pr.gov.br>
2026-06-17 11:39:26 +0000

    Finalizado tratamento de erros com Chalk

--- fs/festa.md
+++ fs/festa.md
@@ -1,13 +1,15 @@
 
 ### Convidados da Minha Festa:
 **amigos especiais**
-- Chester
-- Freddie
-- Bruce
-- Dave
-- Amy  
-- Anthony
-- James
-- Ozzy
-- Dio
+- Chester Bennignton
+- Freddie Mercurie
+- Bruce Dickinson
+- Dave Grohl
+- Amy Lee
+- Anthony Kiedis
+- James Hetfield
+- Ozzy Osbourne
+- Ronnie James Dio
+- Serj Tankian
+- 
 *Entrada apenas com ingressos Eventim*
--- fs/mod-fs-path.js
+++ fs/mod-fs-path.js
@@ -1,16 +1,18 @@
 
 const fs = require('fs')
 const path = require('path')
-const chalk = require('chalk').default
+const chalk = require('chalk').default // Padrão
 
 try {
-    //            módulo-fs                diretório   arquivo
+    //            módulo-fs                diretório   arquivo   codificação
 const arquivo = fs.readFileSync(path.join(__dirname'/festa.md'),'utf-8')
 console.log(chalk.green('Arquivo carregado com sucesso!'))
 console.log(arquivo)
 
-} catch(erro) {
-console.log(chalk.red("Deu ruim:")
+} catch(erro)
+console.log (chalk) {
+console.log(chalk.yellow('Atenção, erro a frente'))
+console.log(chalk.red("Erro 1406:"))
 console.log(erro.mensage)
 
 }
